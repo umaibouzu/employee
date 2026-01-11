83eb4150f14cf6d92f242c59eb5de14e60cb6b1e
umaibouzu <cumboa2003@gmail.com>
2026-01-11 10:34:56 -0600

    Added database headers and parsing

--- src/main.c
+++ src/main.c
@@ -18,7 +18,9 @@ int main(int argc, char *argv[]) {
 	char *filepath = NULL;
 	bool newfile = false;
 	int c;
+
 	int dbfd = -1;
+	struct dbheader_t *dbhdr = NULL;
 
 	while ((c = getopt(argc, argv, "nf:")) != -1) {
 		switch (c) {
@@ -47,16 +49,24 @@ int main(int argc, char *argv[]) {
 			printf("Unable to create database file\n");
 			return -1;
 		}
+		if (create_db_header(dbfd, &dbhdr) == STATUS_ERROR) {
+			printf("Failed to create database header\n");
+			return -1;
+		}
 	}else {
 		dbfd = open_db_file(filepath);
 		if (dbfd == STATUS_ERROR) {
 			printf("Unable to open database file\n");
 			return -1;
 		}
+
+		if (validate_db_header(dbfd, &dbhdr) == STATUS_ERROR) {
+			printf("Failed to validate database header\n");
+			return -1;
+		}
 	}
 
-	printf("Newfile: %d\n", newfile);
-	printf("Filepath: %s\n", filepath);
+	output_file(dbfd, dbhdr);
 
 	return 0;
 }
