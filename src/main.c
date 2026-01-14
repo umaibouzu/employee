af2e5dd9dec9bc45dba622daf3c7f863913a1344
umaibouzu <cumboa2003@gmail.com>
2026-01-13 21:03:59 -0600

    Small change

--- src/main.c
+++ src/main.c
@@ -11,8 +11,9 @@ void print_usage(char *argv[]) {
 	printf("Usage: %s -n -f <database file>\n", argv[0]);
 	printf("\t -n - create new database file\n");
 	printf("\t -f - (required) path to database file\n");
-	printf("\t -l - list the employees\n");
 	printf("\t -a - add via CSV list of (name,address,salary)\n");
+	printf("\t -r - remove employee via name)\n");
+	printf("\t -l - list the employees\n");
 	return;
 }
 
